648d53c219e484ca588edcb050429c2e4773d332
Trevis Jontaé Fields <tfields3@student.rcc.edu>
2017-06-09 01:24:47 -0700

    Chapter Section spacing
    Spacing out of Chapter sections

--- LetsLearnSwifty.playground/Contents.swift
+++ LetsLearnSwifty.playground/Contents.swift
@@ -14,7 +14,7 @@ var _userAge: Int
 
 // Print out statements in Swift that allow display to the console
 print("Hello, " + _programmerName + " is the main programmer for this swift code project.")
-print("They are ",terminator: "")
+print("They are ", terminator: "")
 
 // Assign the number 24 to the variable _userAge
 _userAge = 24 // <-- assigning value of 24 to _userAge
@@ -29,7 +29,7 @@ let _year = 1992
 // Display messages to the user
 print("Their birthday is on ",terminator: "")
 print(_month9, terminator: ", ")
-print(_day,terminator: ", ")
+print(_day, terminator: ", ")
 print(_year)
 
 // Reassign _userAge with a new value of 25
@@ -37,15 +37,19 @@ _userAge = 25
 
 // Display message to the user after reassinging _userAge the value of 25
 print("On", terminator: " ")
-print(_month9,terminator:" ")
+print(_month9, terminator:" ")
 print(_day, terminator:" ")
-print("2017, the user will be",terminator:" ")
+print("2017, the user will be", terminator:" ")
 
 
 // Print out _userAge with the new value
 print(_userAge, " years of age.")
 
 // Chapter 2: Variables
+
+/**********************************
+***********************************
+**********************************/
 
 // Declare _vehicle and _motorcycle
 var _vehicle = "Subaru WRX STI"
@@ -119,6 +123,10 @@ if let _someone = friend
 
 // Chapter 3: Boolean values
 
+/**********************************
+***********************************
+**********************************/
+
 // A data type that tells the truth 
 var truth: Bool
 truth = true
@@ -128,52 +136,52 @@ fake = false
 // Lets ask a question to see if its going to be true or false
 let _test1 = 5 < 3
 
-print("True or False, 5 < 3:",_test1)
+print("True or False, 5 < 3:", _test1)
 let _test2 = 10 > 2
-print("True or False, 10 > 2:",_test2)
+print("True or False, 10 > 2:", _test2)
 
 // Lets check if a value is greater than or equal to
 let _test3 = 25
 let myBool1 = _test3 >= 26 // myBool checks to see if it is true or false
-print("True or False, 25 is greater than or equal to 26:",myBool1)
+print("True or False, 25 is greater than or equal to 26:", myBool1)
 
 let _test4 = 40
 let myBool2 = _test4 >= 39
-print("True or False, 40 is greater than or equal to 39:",myBool2)
+print("True or False, 40 is greater than or equal to 39:", myBool2)
 
 // Lets check if a value is equal to another
 let _test5 = 3
 let myBool3 = _test5 == 3
-print("True or False, 3 is equal to 3:",myBool3)
+print("True or False, 3 is equal to 3:", myBool3)
 
 let _test6 = 9
 let myBool4 = _test6 == 5
-print("True or False, 9 is equal to 5:",myBool4)
+print("True or False, 9 is equal to 5:", myBool4)
 
 // Lets check if a value is not equal to another
 let myBool5 = 20 != 30
-print("True or False, 20 is not equal to 30:",myBool5)
+print("True or False, 20 is not equal to 30:", myBool5)
 
 // Lets check to see if the truth is going to be told or not in this next snippet of code
 let _jarvisTruth = true
 let theTruth = !_jarvisTruth
-print("Did he tell the truth?:",theTruth)
+print("Did he tell the truth?:", theTruth)
 
 // && (logical and) will return the correct value if both tests are the same
 let _bool1 = true
 let _bool2 = false
 let _result = _bool1 && _bool2
-print("Are the two tests equal:",_result)
+print("Are the two tests equal:", _result)
 
 // || (logical or) works like && ot test if either test is true or false
 let _bool3 = true
 let _bool4 = false
 let result = _bool3 || _bool4
-print("This two are not the same:",result)
+print("This two are not the same:", result)
 
 // At its core a boolean value is going to either print a 1 for true or a 0 for false
 let testBool1 = false
-print("A false boolean's hash value prints:",testBool1.hashValue)
+print("A false boolean's hash value prints:", testBool1.hashValue)
 
 // Time for another snippet of code to test if both requirements are met
 let house = "Myers Park Apartments"
@@ -181,6 +189,12 @@ let address = 24420
 let zipCode = house == "Myers Park Apartments"
 let postalCode = address >= 24420
 let mailing = zipCode && postalCode
-print("This will test if everything in the code is identical and will print",mailing, "if it is true.")
+print("This will test if everything in the code is identical and will print", mailing, "if it is true.")
+
+// Chapter 4:
+
+/**********************************
+***********************************
+**********************************/
 
 
